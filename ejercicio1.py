76b425324f845249eea6e37d9f7fe16deca9a877
Jerosoto1 <jerosoto2410@gmail.com>
2025-04-23 08:52:56 -0500

    version-1

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+numero=int (input("dame cualquier numero: "))
+
+if numero % 3 == 0 and numero % 5 == 0:
+    print("FizzBuzz")
+elif numero % 3 == 0: 
+    print("fizz",)
+elif numero % 5 == 0:
+    print("buzz",)
+else: 
+    print(numero)
---
+++ ejercicio2.py
@@ -0,0 +1,6 @@
+edad = int(input("¿Cuantos años tienes?"))
+pase = input("¿Tienes pase dorado?:")
+
+if edad < 18:
+    print
+elif pase == "si":
